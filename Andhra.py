87e535403e44f2bb86983b7cc76ed4930cc16ad1
rkeshava512-byte <rkeshava512@gmail.com>
2026-07-31 12:35:33 +0530

    Add files via upload

---
+++ Andhra.py
@@ -0,0 +1,13 @@
+str1="Welcome to Andhra"
+print(str1)
+print(type(str1))
+#str1[0]=''Hello''
+#print(str1)
+
+str2=str1.split()
+print(str2)
+print(str1.upper())
+print(str1.lower())
+print(str1.count('e'))
+print(str1.capitalize())
+print(str1.rfind('o'))
